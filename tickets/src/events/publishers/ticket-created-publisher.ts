c20ac624dde7fb8e64c1f1552813694f0d3f7dc2
oleg ilenkov <sephiroth.by@gmail.com>
2022-08-17 21:49:13 +0300

    orders service added anc connected to nats

--- tickets/src/events/publishers/ticket-created-publisher.ts
+++ tickets/src/events/publishers/ticket-created-publisher.ts
@@ -1,5 +1,5 @@
 import { Publisher, Subjects, TicketCreatedEvent } from '@septickets/common-v2';
 
 export class TicketCreatedPublisher extends Publisher<TicketCreatedEvent> {
-  readonly subject = Subjects.TicketCreated;
+  subject: Subjects.TicketCreated = Subjects.TicketCreated;
 }
